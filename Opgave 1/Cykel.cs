6bf443618bfd5b58d5e15790ee8c5ffe19a10506
Tristan Taylor Kamp <tris0032@edu.easj.dk>
2020-09-28 10:50:11 +0200

    hej

---
+++ Opgave 1/Cykel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave_1
+{
+    public class Cykel
+    {
+        private int _id;
+        private string _color;
+        private double _price;
+        private int _gear;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set {if (value.Length<1) throw new ArgumentException(); 
+                _color = value; }
+        }
+
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException(); 
+                _price = value; }
+        }
+
+
+
+        public int Gear
+        {
+            get { return _gear; }
+            set { if(value<3 || value >32) throw new ArgumentOutOfRangeException();
+                _gear = value; }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Opgave 1Tests/CykelTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave_1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave_1.Tests
+{
+    [TestClass()]
+    public class CykelTests
+    {
+        [TestMethod()]
+        public void TestTooLowValues()
+        {
+            //Arrange
+
+            Cykel aCykel = new Cykel();
+
+            // Act 
+
+            aCykel.Id = 1;
+
+            // Assert
+
+            Assert.ThrowsException<ArgumentException>(()=>aCykel.Color = "");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=>aCykel.Price = -1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=>aCykel.Gear = 0);
+
+        }
+
+        [TestMethod()]
+        public void TestTooHighValues()
+        {
+            //Arrange
+
+            Cykel aCykel = new Cykel();
+
+            // Act 
+
+            aCykel.Id = 1;
+
+            // Assert
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => aCykel.Gear = 50);
+
+        }
+
+        [TestMethod()]
+        public void TestCorrectValues()
+        {
+            //Arrange
+
+            Cykel aCykel = new Cykel();
+
+            // Act 
+
+            aCykel.Id = 1;
+
+            // Assert
+
+            aCykel.Color = "red";
+            aCykel.Price = 100;
+            aCykel.Gear = 7;
+
+        }
+    }
+}
